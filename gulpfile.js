d6cfaf600216733fbf859b4e77ed2bdd60cfcce2
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-21 13:10:53 -0500

    Initiated the project with some boilerplate.

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require("gulp")
+var browserify = require("browserify")
+
+var del = require("del")
+var vinyl_buffer = require("vinyl-buffer")
+var vinyl_source = require("vinyl-source-stream")
+
+gulp.task("default", ["build"])
+
+gulp.task("build", function() {
+    del(["./build"], function() {
+        gulp.start([
+            "build:markup",
+            "build:scripts",
+            "build:styles",
+            "build:stuffs"
+        ])
+    })
+})
+
+gulp.task("build:markup", function() {
+    gulp.src("./source/index.html")
+        .pipe(gulp.dest("./build"))
+})
+
+gulp.task("build:scripts", function() {
+    browserify("./source/index.js").bundle()
+        .pipe(vinyl_source("index.js"))
+        .pipe(gulp.dest("./build"))
+})
+
+gulp.task("build:styles", function() {
+    gulp.src("./source/index.css")
+        .pipe(gulp.dest("./build"))
+})
+
+gulp.task("build:stuffs", function() {
+    gulp.src(["./source/*", "!./source/*.html",
+              "!./source/*.js", "!./source/*.css"])
+        .pipe(gulp.dest("./build"))
+})
+
+gulp.task("watch", function() {
+    gulp.watch("./source/**/*", function() {
+        gulp.start("build")
+    })
+})
---
+++ source/index.js
@@ -0,0 +1,3 @@
+var jQuery = require("jQuery")
+
+jQuery("#index").html("Hello World!")
